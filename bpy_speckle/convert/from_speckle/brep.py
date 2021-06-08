03ec9e30ae748fc3f59b331decfcb8058e168948
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-06-08 15:03:48 +0100

    fix(convert): check display val & mesh for breps

--- bpy_speckle/convert/from_speckle/brep.py
+++ bpy_speckle/convert/from_speckle/brep.py
@@ -7,14 +7,14 @@ def import_brep(speckle_brep, scale, name=None):
     if not name:
         name = speckle_brep.geometryHash or speckle_brep.id
 
-    if speckle_brep.displayValue:
-
+    display = speckle_brep.displayMesh or speckle_brep.displayValue
+    if display:
         if name in bpy.data.meshes.keys():
             mesh = bpy.data.meshes[name]
         else:
             mesh = bpy.data.meshes.new(name=name)
 
-        to_bmesh(speckle_brep.displayValue, mesh, name, scale)
+        to_bmesh(display, mesh, name, scale)
         # add_custom_properties(speckle_brep[dvKey], mesh)
     else:
         mesh = None
