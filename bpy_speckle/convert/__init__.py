0a2c1f9f32e5273f7f899a7a1ebf0a59313f0d59
izzylys <izzy.lyseggen@gmail.com>
2022-01-10 09:26:22 -0800

    refactor(convert): remove unused methods

--- bpy_speckle/convert/__init__.py
+++ bpy_speckle/convert/__init__.py
@@ -1,25 +1,4 @@
-from mathutils import Matrix
 from bpy_speckle.convert.to_native import convert_to_native
-
-
-def set_transform(speckle_object, blender_object):
-    transform = None
-    if hasattr(speckle_object, "transform"):
-        transform = speckle_object.transform
-    elif (
-        hasattr(speckle_object, "properties") and speckle_object.properties is not None
-    ):
-        transform = speckle_object.properties.get("transform", None)
-
-    if transform and len(transform) == 16:
-        mat = Matrix([transform[:4], transform[4:8], transform[8:12], transform[12:16]])
-
-        blender_object.matrix_world = mat
-
-
-def try_add_property(speckle_object, blender_object, prop, prop_name):
-    if prop in speckle_object.keys() and speckle_object[prop] is not None:
-        blender_object[prop_name] = speckle_object[prop]
 
 
 def get_speckle_subobjects(attr, scale, name):
