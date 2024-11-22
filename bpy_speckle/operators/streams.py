9ba213da3212d0c668c90b29869e92c286dc5784
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2024-11-22 11:51:54 +0000

    Fixed issue with local server builds

--- bpy_speckle/operators/streams.py
+++ bpy_speckle/operators/streams.py
@@ -45,9 +45,17 @@ ReceiveCompleteCallback = Optional[
 
 def get_project_workspace_id(client: SpeckleClient, project_id: str) -> Optional[str]:
     workspace_id = None
-    server_version = client.project.server_version or client.server.verison()
-    maj = server_version[0]
-    min = server_version[1]
+    server_version = client.project.server_version or client.server.version()
+
+    # Local yarn builds of server will report a server version if "dev"
+    # We'll assume that local builds are up-to-date with the latest features
+    if server_version[0] == "dev":
+        maj = 999
+        min = 999
+    else:
+        maj = server_version[0]
+        min = server_version[1]
+
     if maj > 2 or (maj == 2 and min > 20):
         workspace_id = client.project.get(project_id).workspaceId
     return workspace_id
