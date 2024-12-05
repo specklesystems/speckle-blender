fe69091c5c5d8cd7fb91b745ae4bd30d6fddb455
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2024-12-05 15:13:26 +0300

    added type hints for account manager functions

--- bpy_speckle/utils/account_manager.py
+++ bpy_speckle/utils/account_manager.py
@@ -1,7 +1,7 @@
 from specklepy.api.credentials import get_local_accounts
 
-def get_account_enum_items():
+def get_account_enum_items() -> list[tuple[str, str, str]]:
     return [(acc.id, f"{acc.userInfo.name} - {acc.serverInfo.url} - {acc.userInfo.email}", "") for acc in get_local_accounts()]
 
-def get_default_account_id():
+def get_default_account_id() -> str | None:
     return next((acc.id for acc in get_local_accounts() if acc.isDefault), None)
