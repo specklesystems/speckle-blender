d2392b0d2b63d5e0e8455aa37cb8d666a75c3b0e
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2025-01-17 18:37:48 +0300

    type checking in publish py

--- bpy_speckle/operators/publish.py
+++ bpy_speckle/operators/publish.py
@@ -1,4 +1,6 @@
 import bpy
+from bpy.types import Context
+from bpy.types import Event
 
 # Publish Operator
 class SPECKLE_OT_publish(bpy.types.Operator):
@@ -7,11 +9,11 @@ class SPECKLE_OT_publish(bpy.types.Operator):
     bl_label = "Publish to Speckle"
     bl_description = "Publish selected objects to Speckle"
 
-    def invoke(self, context, event):
+    def invoke(self, context: Context, event: Event) -> set:
         context.scene.speckle_state.mouse_position = (event.mouse_x, event.mouse_y)
         return self.execute(context)
 
-    def execute(self, context):
+    def execute(self, context: Context) -> set:
         context.scene.speckle_state.ui_mode = "PUBLISH"
         self.report({'INFO'}, f"Publish button clicked at {context.scene.speckle_state.mouse_position[0], context.scene.speckle_state.mouse_position[1]}")
         bpy.ops.speckle.project_selection_dialog("INVOKE_DEFAULT")
