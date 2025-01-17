c4061182f936da42d54b26fe8e4d83fdca5230c5
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2025-01-17 21:08:19 +0300

    added type checking to model card settings

--- bpy_speckle/operators/model_card_settings.py
+++ bpy_speckle/operators/model_card_settings.py
@@ -1,5 +1,7 @@
 import bpy
 import webbrowser
+from typing import Set
+from bpy.types import Event, Context, UILayout
 
 class SPECKLE_OT_model_card_settings(bpy.types.Operator):
     """
@@ -13,17 +15,18 @@ class SPECKLE_OT_model_card_settings(bpy.types.Operator):
     bl_description = "Settings for the model card"
     model_name: bpy.props.StringProperty()
 
-    def execute(self, context):
+    def execute(self, context: Context) -> Set[str]:
         self.report({'INFO'}, f"Settings for {self.model_name}")
         return {'FINISHED'}
     
-    def draw(self, context):
-        layout = self.layout
+    def draw(self, context: Context) -> None:
+        layout: UILayout = self.layout
         # Add a button for viewing 3d model in the browser
         layout.operator("speckle.view_in_browser", text="View in Browser")
         # Add a button for viewing model versions in the browser
         layout.operator("speckle.view_model_versions", text="View Model Versions")
-    def invoke(self, context, event):
+
+    def invoke(self, context: Context, event: Event) -> Set[str]:
         wm = context.window_manager
         return wm.invoke_props_dialog(self)
 
@@ -36,7 +39,7 @@ class SPECKLE_OT_view_in_browser(bpy.types.Operator):
     bl_label = "View in Browser"
     bl_description = "View the model in the browser"
 
-    def execute(self, context):
+    def execute(self, context: Context) -> Set[str]:
         # TODO: Update this to model URL
         webbrowser.open(f"https://speckle.guide")
         self.report({'INFO'}, f"Viewing in the browser")
@@ -51,7 +54,7 @@ class SPECKLE_OT_view_model_versions(bpy.types.Operator):
     bl_label = "View Model Versions"
     bl_description = "View the model versions in the browser"
 
-    def execute(self, context):
+    def execute(self, context: Context) -> Set[str]:
         # TODO: Update this to model versions URL
         webbrowser.open(f"https://speckle.guide")
         self.report({'INFO'}, f"Viewing model's versions in the browser")
