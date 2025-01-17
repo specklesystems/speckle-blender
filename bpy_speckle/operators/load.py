d1dcf8635775d92d0577455804883ffc4210fc93
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2025-01-17 21:03:38 +0300

    added type hinting to load.py

--- bpy_speckle/operators/load.py
+++ bpy_speckle/operators/load.py
@@ -1,4 +1,5 @@
 import bpy
+from typing import Set
 
 # Load Operator
 class SPECKLE_OT_load(bpy.types.Operator):
@@ -6,11 +7,11 @@ class SPECKLE_OT_load(bpy.types.Operator):
     bl_label = "Load from Speckle"
     bl_description = "Load objects from Speckle"
 
-    def invoke(self, context, event):
+    def invoke(self, context: bpy.types.Context, event: bpy.types.Event) -> Set[str]:
         context.scene.speckle_state.mouse_position = (event.mouse_x, event.mouse_y)
         return self.execute(context)
 
-    def execute(self, context):
+    def execute(self, context: bpy.types.Context) -> Set[str]:
         context.scene.speckle_state.ui_mode = "LOAD"
         self.report({'INFO'}, f"Load button clicked at {context.scene.speckle_state.mouse_position[0], context.scene.speckle_state.mouse_position[1]}")
         bpy.ops.speckle.project_selection_dialog("INVOKE_DEFAULT")
