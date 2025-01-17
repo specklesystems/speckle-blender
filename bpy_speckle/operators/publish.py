70d52db17f5d67ba6f0ed4d59f49599ac99ff3a2
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2025-01-17 21:03:29 +0300

    import typing

--- bpy_speckle/operators/publish.py
+++ bpy_speckle/operators/publish.py
@@ -1,6 +1,7 @@
 import bpy
 from bpy.types import Context
 from bpy.types import Event
+from typing import Set
 
 # Publish Operator
 class SPECKLE_OT_publish(bpy.types.Operator):
@@ -9,11 +10,11 @@ class SPECKLE_OT_publish(bpy.types.Operator):
     bl_label = "Publish to Speckle"
     bl_description = "Publish selected objects to Speckle"
 
-    def invoke(self, context: Context, event: Event) -> set:
+    def invoke(self, context: Context, event: Event) -> Set[str]:
         context.scene.speckle_state.mouse_position = (event.mouse_x, event.mouse_y)
         return self.execute(context)
 
-    def execute(self, context: Context) -> set:
+    def execute(self, context: Context) -> Set[str]:
         context.scene.speckle_state.ui_mode = "PUBLISH"
         self.report({'INFO'}, f"Publish button clicked at {context.scene.speckle_state.mouse_position[0], context.scene.speckle_state.mouse_position[1]}")
         bpy.ops.speckle.project_selection_dialog("INVOKE_DEFAULT")
