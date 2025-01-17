9635f04db83988dd76f0738bbdba3c07229ceecf
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2025-01-17 22:13:03 +0300

    type checking in model_card py

--- bpy_speckle/ui/model_card.py
+++ bpy_speckle/ui/model_card.py
@@ -1,19 +1,19 @@
 import bpy
 
 class speckle_model_card(bpy.types.PropertyGroup):
-    project_name: bpy.props.StringProperty(name="Project Name", description="Name of the project", default="")
-    model_name: bpy.props.StringProperty(name="Model Name", description="Name of the model", default="")
-    is_publish: bpy.props.BoolProperty(name="Publish/Load", description="If the model is published or loaded", default=False)
-    selection_summary: bpy.props.StringProperty(name="Selection Summary", description="Summary of the selection", default="")
-    version_id: bpy.props.StringProperty(name="Version ID", description="ID of the selected version", default="")
+    project_name: bpy.props.StringProperty(name="Project Name", description="Name of the project", default="")  # type: ignore
+    model_name: bpy.props.StringProperty(name="Model Name", description="Name of the model", default="")  # type: ignore
+    is_publish: bpy.props.BoolProperty(name="Publish/Load", description="If the model is published or loaded", default=False)  # type: ignore
+    selection_summary: bpy.props.StringProperty(name="Selection Summary", description="Summary of the selection", default="")  # type: ignore
+    version_id: bpy.props.StringProperty(name="Version ID", description="ID of the selected version", default="")  # type: ignore
 
     def to_dict(self):
-        return{
-            "project_name" : self.project_name,
-            "model_name" : self.model_name,
-            "is_publish" : self.is_publish,
-            "selection_summary" : self.selection_summary,
-            "version_id" : self.version_id,
+        return {
+            "project_name": self.project_name,
+            "model_name": self.model_name,
+            "is_publish": self.is_publish,
+            "selection_summary": self.selection_summary,
+            "version_id": self.version_id,
         }
     
     @classmethod
