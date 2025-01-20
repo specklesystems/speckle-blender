9301186b63c14e2d3827cb4bfd5a6ed1c22050c0
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2025-01-20 15:02:16 +0300

    add docstrings to speckle_state

--- bpy_speckle/states/speckle_state.py
+++ bpy_speckle/states/speckle_state.py
@@ -5,6 +5,17 @@ from bpy.types import PropertyGroup
 from ..ui.model_card import speckle_model_card
 
 class SpeckleState(PropertyGroup):
+    """Manages the state of the Speckle addon in Blender.
+
+    This class stores UI-related state information for the Speckle addon, including
+    the current UI mode, model cards collection, and UI interaction states.
+
+    Attributes:
+        ui_mode (StringProperty): Current UI mode of the addon. Defaults to "NONE".
+        model_cards (CollectionProperty): Collection of Speckle model cards.
+        model_card_index (IntProperty): Index of the currently selected model card.
+        mouse_position (IntVectorProperty): 2D vector storing current mouse position.
+    """
     ui_mode: StringProperty(name="UI Mode", default="NONE")  # type: ignore
     model_cards: CollectionProperty(type=speckle_model_card)  # type: ignore
     model_card_index: IntProperty(name="Model Card Index", default=0)  # type: ignore
