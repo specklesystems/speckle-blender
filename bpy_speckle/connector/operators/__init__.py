292e46ac31ce427c9c34f3084765c3a491686b6f
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2025-02-25 21:06:34 +0300

    remove ruff warnings

--- bpy_speckle/connector/operators/__init__.py
+++ bpy_speckle/connector/operators/__init__.py
@@ -1,2 +1,4 @@
+__all__ = ["SPECKLE_OT_load", "SPECKLE_OT_publish"]
+
 from .load import SPECKLE_OT_load
 from .publish import SPECKLE_OT_publish
--- bpy_speckle/connector/ui/__init__.py
+++ bpy_speckle/connector/ui/__init__.py
@@ -1,1 +1,3 @@
+__all__ = ["SPECKLE_PT_main_panel"]
+
 from .main_panel import SPECKLE_PT_main_panel
