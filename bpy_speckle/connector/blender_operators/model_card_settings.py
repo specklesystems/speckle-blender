0dbf691f16c890cfcbaf462aea13866576dbedee
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2025-03-26 16:49:02 +0300

    adds delete model card button

--- bpy_speckle/connector/blender_operators/model_card_settings.py
+++ bpy_speckle/connector/blender_operators/model_card_settings.py
@@ -25,6 +25,11 @@ class SPECKLE_OT_model_card_settings(bpy.types.Operator):
         layout.operator("speckle.view_in_browser", text="View in Browser").model_card_index = self.model_card_index
         # Add a button for viewing model versions in the browser
         layout.operator("speckle.view_model_versions", text="View Model Versions").model_card_index = self.model_card_index
+        # Add a separator
+        layout.separator()
+        # Add a button for deleting the model card
+        delete_op = layout.operator("speckle.delete_model_card", text="Delete Model Card", icon='TRASH')
+        delete_op.model_card_index = self.model_card_index
 
     def invoke(self, context: Context, event: Event) -> Set[str]:
         wm = context.window_manager
@@ -70,3 +75,32 @@ class SPECKLE_OT_view_model_versions(bpy.types.Operator):
 
         self.report({'INFO'}, "Viewing model's versions in the browser")
         return {'FINISHED'}
+
+# Operator for deleting a model card
+class SPECKLE_OT_delete_model_card(bpy.types.Operator):
+    """Deletes a Speckle model card from the Blender UI.
+
+    This operator removes a model card from the collection of model cards
+    in the Speckle state after confirming with the user.
+    """
+    bl_idname = "speckle.delete_model_card"
+    bl_label = "Delete Model Card"
+    bl_description = "Delete this model card"
+
+    model_card_index: bpy.props.IntProperty() #type: ignore
+
+    def execute(self, context: Context) -> Set[str]:
+        # Get the model card name for the report message
+        model_card = context.scene.speckle_state.model_cards[self.model_card_index]
+        model_name = model_card.model_name
+        
+        # Remove the model card from the collection
+        context.scene.speckle_state.model_cards.remove(self.model_card_index)
+        
+        # Report success
+        self.report({'INFO'}, f"Model card '{model_name}' has been deleted")
+        return {'FINISHED'}
+    
+    def invoke(self, context: Context, event: Event) -> Set[str]:
+        # Show a confirmation dialog
+        return context.window_manager.invoke_confirm(self, event)
