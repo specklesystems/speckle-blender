b22d94606cb2d82fe60f225818773fb18950ef2c
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2025-04-10 17:46:18 +0300

    add load operation to load button

--- bpy_speckle/connector/blender_operators/load_button.py
+++ bpy_speckle/connector/blender_operators/load_button.py
@@ -1,6 +1,7 @@
 import bpy
 from typing import Set
 from bpy.types import Context
+from ..operations.load_operation import load_operation
 
 
 class SPECKLE_OT_load(bpy.types.Operator):
@@ -9,19 +10,12 @@ class SPECKLE_OT_load(bpy.types.Operator):
     bl_description = "Load objects from Speckle"
 
     def invoke(self, context: Context, event: bpy.types.Event) -> Set[str]:
-        # Captures cursor position for UI placement
-        context.scene.speckle_state.mouse_position = (event.mouse_x, event.mouse_y)
         return self.execute(context)
 
     def execute(self, context: Context) -> Set[str]:
         # Sets the UI mode to LOAD
         context.scene.speckle_state.ui_mode = "LOAD"
-        # Logs cursor position
-        self.report(
-            {"INFO"},
-            f"Load button clicked at {context.scene.speckle_state.mouse_position[0], context.scene.speckle_state.mouse_position[1]}",
-        )
-        # Opens project_selection_dialog
-        bpy.ops.speckle.project_selection_dialog("INVOKE_DEFAULT")
+        # Load selected model version
+        load_operation(context)
 
         return {"FINISHED"}
