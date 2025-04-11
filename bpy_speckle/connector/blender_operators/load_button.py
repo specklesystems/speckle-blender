b2d9aba7786c5f87091f9e8d2007e591ae722eb9
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2025-04-11 22:16:02 +0300

    load button adds model cards

--- bpy_speckle/connector/blender_operators/load_button.py
+++ bpy_speckle/connector/blender_operators/load_button.py
@@ -13,6 +13,17 @@ class SPECKLE_OT_load(bpy.types.Operator):
         return self.execute(context)
 
     def execute(self, context: Context) -> Set[str]:
+        wm = context.window_manager
+        model_card = context.scene.speckle_state.model_cards.add()
+        model_card.server_url = get_server_url_by_account_id(wm.selected_account_id)
+        model_card.project_id = wm.selected_project_id
+        model_card.project_name = wm.selected_project_name
+        model_card.model_id = wm.selected_model_id
+        model_card.model_name = wm.selected_model_name
+        model_card.is_publish = False
+        model_card.load_option = wm.selected_version_load_option
+        model_card.version_id = wm.selected_version_id
+
         # Load selected model version
         load_operation(context)
 
