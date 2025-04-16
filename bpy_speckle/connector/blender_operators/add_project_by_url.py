ae3222683ea095fcdb2a1971238d100d7d7ee69c
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2025-04-16 19:30:30 +0300

    get model details by wrapper

--- bpy_speckle/connector/blender_operators/add_project_by_url.py
+++ bpy_speckle/connector/blender_operators/add_project_by_url.py
@@ -31,4 +31,22 @@ def register() -> None:
 
 def unregister() -> None:
     bpy.utils.unregister_class(SPECKLE_OT_add_project_by_url)
-    
+
+def get_model_details_by_wrapper(wrapper: StreamWrapper) -> Tuple[str, str, str, str, str, str]:
+    client = wrapper.get_client()
+    client.authenticate_with_account(wrapper.get_account())
+    account_id, project_id, project_name, model_id, model_name, version_id, load_option = "", "", "", "", "", "", ""
+    account_id = wrapper.get_account().id
+    if wrapper.stream_id:
+        project_id = wrapper.stream_id
+        project = client.project.get(project_id)
+        project_name = project.name
+    if wrapper.model_id:
+        model_id = wrapper.model_id
+        model = client.model.get(model_id, project_id)
+        model_name = model.name
+        load_option = "LATEST"
+    if wrapper.commit_id:
+        version_id = wrapper.commit_id
+        load_option = "SPECIFIC"
+    return (account_id, project_id, project_name, model_id, model_name, version_id, load_option)
